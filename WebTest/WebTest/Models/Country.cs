0e8bdf6b307416af76c1a5c4c124c52cfb5f6d73
Ziv Cohen <ziv.cohen.3456@gmail.com>
2018-10-21 20:56:28 +0300

    Initial two models

---
+++ WebTest/WebTest/Models/Country.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebTest.Models
+{
+    public class Country
+    {
+        public int CountryID { get; set; }
+        public string Name { get; set; }
+        public string Language { get; set; }
+        public string Currency { get; set; }
+    }
+}
---
+++ WebTest/WebTest/Models/Site.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebTest.Models
+{
+    public class Site
+    {
+        public int SiteID { get; set; }
+        public string Name { get; set; }
+        public int Rank { get; set; }
+        public string Location { get; set; }
+        public int Pistes { get; set; }
+        public int Difficulty { get; set; }
+        public double BeerPrice { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:MMM d}", ApplyFormatInEditMode = true)]
+        public DateTime SeasonStart { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:MMM d}", ApplyFormatInEditMode = true)]
+        public DateTime SeasonEnd { get; set; }
+    }
+}
