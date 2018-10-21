9d5263db30589e2202d9a78d7179c1905654ab69
Ziv Cohen <ziv.cohen.3456@gmail.com>
2018-10-21 22:05:30 +0300

    Add model SkiContext for DB sets: Countries and Sites

---
+++ WebTest/WebTest/Models/SkiContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebTest.Models
+{
+    public class SkiContext : DbContext
+    {
+        public DbSet<Country> Countries { get; set; }
+        public BdSet<Site> Sites { get; set; }
+    }
+}
